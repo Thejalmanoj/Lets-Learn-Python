adc4690613efeae503465e0c0ab8912cf86605b6
Thejal <76029279+Thejalmanoj@users.noreply.github.com>
2026-04-10 18:51:22 +0530

    valid_mountain.py

--- valid_mountain.py
+++ valid_mountain.py
@@ -1,3 +1,8 @@
+#Question: Given an array of integers arr, return true if and only if it is a valid mountain array.
+#Logic: array length should be greater than or equal to 3. There exists some i with 0 < i < arr.length - 1 such that:
+                        # arr[0] < arr[1] < ... < arr[i - 1] < arr[i] 
+                        # arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
 arr=list(map(int,input().strip().split(" ")))
 climb=0
 if(len(arr)<3):
