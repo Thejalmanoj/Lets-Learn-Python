e517725ada1199d3f13def2381488122639ff4de
Subin Simon <77583980+subinsimon4567@users.noreply.github.com>
2026-04-10 18:41:46 +0530

    Update Runner up score.py

--- Runner up score.py
+++ Runner up score.py
@@ -1,7 +1,7 @@
 #Question: Given the participants’ score sheet for your University Sports Day, you are required to find the runner-up score.
-#You are given n scores. Store them in a list and find the score of the runner-up.
-#Input Format : The first line contains n. The second line contains an array A [] of n integers each separated by a space.
+#Input Format : The first line contains n (Number of participants).The second line contains an array A[] of n integers each separated by a space.
 #Output format : Print the runner-up score.
+# ( Logic: Store the scores in ascending order. Runner up score is the 2nd largest element) 
 
 if __name__ == '__main__':
     n=int(input())
