1446c808b0f4c5e2444399893cd883f8189dc23a
Subin Simon <77583980+subinsimon4567@users.noreply.github.com>
2026-04-10 18:37:32 +0530

    Add comments for runner-up score problem statement
    Added problem statement and input/output format comments.

--- Runner up score.py
+++ Runner up score.py
@@ -1,3 +1,8 @@
+#Question: Given the participants’ score sheet for your University Sports Day, you are required to find the runner-up score.
+#You are given n scores. Store them in a list and find the score of the runner-up.
+#Input Format : The first line contains n. The second line contains an array A [] of n integers each separated by a space.
+#Output format : Print the runner-up score.
+
 if __name__ == '__main__':
     n=int(input())
     arr=set(map(int, input().split()))
